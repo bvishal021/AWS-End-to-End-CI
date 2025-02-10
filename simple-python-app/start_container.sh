5f2ad6dfe55564daac649487e7ccc3cbeae8b736
Vishal Baidoddi <vishalbaidoddi@gmail.com>
2025-02-10 14:33:55 +0530

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull bvishal021/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 bvishal021/simple-python-flask-app
